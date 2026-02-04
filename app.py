6c0f0cb27c7fe6286d3ce4b7da9b2c5ac06c8887
Meluwor <mukeratov@web.de>
2026-02-04 16:15:11 +0100

    created data models for database

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 db.init_app(app)
 
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
+
+# with app.app_context():
+#  db.create_all()
--- data_models.py
+++ data_models.py
@@ -1,15 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Integer, Column, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 
-class Author:
-    pass
-class Book:
-    pass
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    birth_date = Column(Date)
+    date_of_death = Column(Date)
+
+    books = relationship("Book", back_populates="author")
+    def __repr__(self):
+        """
+        A simple info string if needed.
+        """
+        return f"<Author id: {self.id}, name: '{self.name}')>"
+
+
+    def __str__(self):
+        """
+        A simple info string if needed.
+        """
+        birth = self.birth_date.year if self.birth_date else "unknown"
+        death = self.date_of_death.year if self.date_of_death else "unknown"
+        return f"Author: {self.name} birth_date: {birth} date_of_death: {death}"
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    author_id = Column(Integer, ForeignKey('authors.id'))
+    isbn = Column(Integer)
+    title = Column(String)
+    publication_year = Column(Date)
+
+    author = relationship("Author", back_populates="books")
+
+    def __repr__(self):
+        """
+        A simple info string if needed.
+        """
+        return f"<Book id: {self.id}, title: '{self.title}')>"
+
+    def __str__(self):
+        """
+        A simple info string if needed.
+        """
+        return f"<Book id: {self.id}, title: '{self.title}')>"
 
 def main():
     print("start")
+
+
 if __name__ == "__main__":
     main()
